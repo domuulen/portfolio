d7482a55f567926854cda7c628fef6e83cf46b75
Temuulen <u.temuulen0409@gmail.com>
2025-07-31 23:59:28 +0100

    gg

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from "next/app";
-import Head from "next/head";
 import "../styles/globals.css";
 import "../styles/about.css";
 import "../styles/contact.css";
@@ -11,13 +10,6 @@ import "../styles/project.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        {/* Google Fonts - Poppins */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <Component {...pageProps} />
     </>
   );
